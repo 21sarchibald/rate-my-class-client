a6cb2f37552b7d9042ad0fdd6a29602c7ca6466a
Larson04 <lar23016@byui.edu>
2026-07-18 22:21:44 -0600

    Delete review functionality

--- src/components/ReviewCard.svelte
+++ src/components/ReviewCard.svelte
@@ -1,11 +1,32 @@
 <script lang="ts">
     import type { Review } from "../js/types.mts";
+    import auth from "../js/auth.svelte.ts";
+    import { onMount } from "svelte";
+    import {deleteReview} from "../js/reviews.mts";
 
     interface Props {
       review: Review;
     }
 
     let { review }: Props = $props();
+
+    onMount(async () => {
+        auth.checkAuth();
+
+    });
+
+    async function handleDelete() {
+        try {
+            await deleteReview(auth.userStore.token, review._id);
+
+            // Refresh the page (simple solution)
+            window.location.reload();
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
 
 </script>
 
@@ -16,63 +37,69 @@
     <p id="courseCode">{review.courseCode}</p> -->
     <h2>{`${review.courseName} (${review.courseCode})`}</h2>
     <div class="review-detail">
-    <label for="rating">Rating:</label>
-    <p id="rating">{review.rating}</p>
-</div>
+        <label for="rating">Rating:</label>
+        <p id="rating">{review.rating}</p>
+    </div>
     
     <div class="review-detail">
-    <label for="difficulty">Difficulty:</label>
-    <p id="difficulty">{review.difficulty}</p>
-</div>
+        <label for="difficulty">Difficulty:</label>
+        <p id="difficulty">{review.difficulty}</p>
+    </div>
 
     <div class="review-detail">
-    <label for="recommmend">Would Recommend:</label>
-    <p id="recommend">{review.recommend ? "Yes" : "No"}</p>
-</div>
+        <label for="recommmend">Would Recommend:</label>
+        <p id="recommend">{review.recommend ? "Yes" : "No"}</p>
+    </div>
 
     <div class="review-detail">
-    <label for="professor">Professor:</label>
-    <p id="professor">{review.professor}</p>
-</div>
+        <label for="professor">Professor:</label>
+        <p id="professor">{review.professor}</p>
+    </div>
 
     <div class="review-detail">
-    <label for="semester">Semester:</label>
-    <p id="semester">{review.semester}</p>
-</div>
+        <label for="semester">Semester:</label>
+        <p id="semester">{review.semester}</p>
+    </div>
 
     <div class="review-detail">
-    <label for="year">Year:</label>
-    <p id="year">{review.year}</p>
-</div>
+        <label for="year">Year:</label>
+        <p id="year">{review.year}</p>
+    </div>
 
     <div class="review-detail">
-    <label for="courseType">Course Type:</label>
-    <p id="courseType">{review.type}</p>
-</div>
+        <label for="courseType">Course Type:</label>
+        <p id="courseType">{review.type}</p>
+    </div>
 
     <div class="review-detail">
-    <label for="description">Review:</label>
-    <p id="description">{review.description}</p>
-</div>
+        <label for="description">Review:</label>
+        <p id="description">{review.description}</p>
+    </div>
 
     <div class="review-detail">
-    <label for="gradeReceived">Grade Received:</label>
-    <p id="gradeReceived">{review.gradeReceived}</p>
-</div>
+        <label for="gradeReceived">Grade Received:</label>
+        <p id="gradeReceived">{review.gradeReceived}</p>
+    </div>
 
     <div class="review-detail">
-    <label for="likes">Likes:</label>
-    <p id="likes">{review.likes}</p>
-</div>
+        <label for="likes">Likes:</label>
+        <p id="likes">{review.likes}</p>
+    </div>
 
     <div class="review-detail">
-    <label for="dislikes">Dislikes:</label>
-    <p id="dislikes">{review.dislikes}</p>
-</div>
+        <label for="dislikes">Dislikes:</label>
+        <p id="dislikes">{review.dislikes}</p>
+    </div>
 
     <div class="review-detail">
         <label for="date">Date:</label>
         <p id="date">{review.createdAt}</p>
+    </div>
+
+    <div class="delete-button">
+        {#if auth.userStore.user?._id === review.userId.toString() || auth.userStore.user?.userType === "Admin"}
+            <button onclick={() => handleDelete()}>Delete</button>
+        {/if}
     </div>
 </div>
 
@@ -107,6 +134,21 @@
         display: flex;
         flex-direction: row;
         align-items: center;
+    }
+    button {
+        
+        background-color: #6ea7f8;
+        color: #303030;
+        border: none;
+        border-radius: 1rem;
+        padding: 0.5rem 1.5rem;
+        cursor: pointer;
+
+        &:hover{
+            background-color: #486f9e;
+            color: #f4f3f3;
+        }
+
     }
 
   </style>
